6fdf884d393a87144ff32c9b4fd14ea2c6023925
Hiroki Takeda <takedahiroki@gmail.com>
2021-08-29 22:13:34 +0900

    札幌市の新型コロナウイルス感染症日別新規患者数データモデルを追加。

---
+++ ash_unofficial_covid19/models/sapporo_patients_number.py
@@ -0,0 +1,72 @@
+from datetime import date
+
+from ash_unofficial_covid19.models.factory import Factory
+
+
+class SapporoPatientsNumber:
+    """札幌市の新型コロナウイルス感染症の日別新規患者数を表すモデルオブジェクト
+
+    Attributes:
+        publication_date (date): 公表日
+        patients_number (int): 新規患者数
+
+    """
+
+    def __init__(self, publication_date: date, patients_number: int):
+        """
+        Args:
+            publication_date (date): 公表日
+            patients_number (int): 新規患者数
+
+        """
+        self.__publication_date = publication_date
+        self.__patients_number = patients_number
+
+    @property
+    def publication_date(self) -> float:
+        return self.__publication_date
+
+    @property
+    def patients_number(self) -> float:
+        return self.__patients_number
+
+
+class SapporoPatientsNumberFactory(Factory):
+    """札幌市の新型コロナウイルス感染症の日別新規患者数を表すモデルオブジェクトを生成
+
+    Attributes:
+        items (list of :obj:`SapporoPatientsNumber`): 札幌市の日別新規患者数一覧リスト
+            SapporoPatientsNumberクラスのオブジェクトのリスト
+
+    """
+
+    def __init__(self):
+        self.__items = list()
+
+    @property
+    def items(self) -> list:
+        return self.__items
+
+    def _create_item(self, **row) -> SapporoPatientsNumber:
+        """SapporoPatientsNumberオブジェクトの生成
+
+        Args:
+            row (dict): 札幌市の日別新規患者数データの辞書
+                札幌市の新型コロナウイルス感染症日別新規患者数データオブジェクトを
+                作成するための引数
+
+        Returns:
+            sapporo_patients_number (:obj:`SapporoPatientsNumber`): 医療機関データ
+                SapporoPatientsNumberクラスのオブジェクト
+
+        """
+        return SapporoPatientsNumber(**row)
+
+    def _register_item(self, item: SapporoPatientsNumber):
+        """SapporoPatientsNumberオブジェクトをリストへ追加
+
+        Args:
+            item (:obj:`SapporoPatientsNumber`): SapporoPatientsNumberクラスのオブジェクト
+
+        """
+        self.__items.append(item)
---
+++ tests/models/test_sapporo_patients_number.py
@@ -0,0 +1,23 @@
+import unittest
+from datetime import date
+
+from ash_unofficial_covid19.models.sapporo_patients_number import (
+    SapporoPatientsNumber,
+    SapporoPatientsNumberFactory
+)
+
+
+class TestSapporoPatientsNumberFactory(unittest.TestCase):
+    def test_create(self):
+        test_data = {
+            "publication_date": date(2021, 8, 28),
+            "patients_number": 274,
+        }
+        factory = SapporoPatientsNumberFactory()
+        # SapporoPatientsNumberクラスのオブジェクトが生成できるか確認する。
+        sapporo_patients_number = factory.create(**test_data)
+        self.assertTrue(isinstance(sapporo_patients_number, SapporoPatientsNumber))
+
+
+if __name__ == "__main__":
+    unittest.main()
