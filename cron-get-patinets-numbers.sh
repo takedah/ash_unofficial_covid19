101faf835f57085da13e648e3bd45071810a59e1
Hiroki Takeda <takedahiroki@gmail.com>
2022-10-23 18:52:59 +0900

    fixed.

---
+++ cron-get-patinets-numbers.sh
@@ -0,0 +1,3 @@
+#!/bin/bash -l
+
+poetry run python -m ash_unofficial_covid19.import_patients_numbers
---
+++ cron-get-reservation_statuses.sh
@@ -0,0 +1,3 @@
+#!/bin/bash -l
+
+poetry run python -m ash_unofficial_covid19.import_reservation_statuses
