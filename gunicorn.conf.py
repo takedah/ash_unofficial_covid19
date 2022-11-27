286cfefc05597f279793b9bc56cd51c144cb2923
Hiroki Takeda <takedahiroki@gmail.com>
2022-11-27 12:00:50 +0900

    fixed.

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,5 +1,16 @@
+import multiprocessing
+
+
 def when_ready(server):
     open("/tmp/app-initialized", "w").close()
 
 
 bind = "unix:///var/gunicorn/nginx.socket"
+reload = True
+workers = multiprocessing.cpu_count() * 2 + 1
+worker_connections = 1024
+backlog = 2048
+max_requests = 5120
+timeout = 120
+accesslog = "./logs/access.log"
+errorlog = "./logs/error.log"
