d5a29e354f76937124a534f275ea48c465a1a1e2
Hiroki Takeda <takedahiroki@gmail.com>
2023-06-10 09:17:20 +0900

    北海道ホームページから旭川市の発熱外来ExcelファイルのURLを抽出する処理を追加する。

---
+++ ash_unofficial_covid19/scrapers/outpatient_link.py
@@ -0,0 +1,66 @@
+import re
+
+from bs4 import BeautifulSoup
+
+from ..scrapers.downloader import DownloadedHTML
+from ..scrapers.scraper import Scraper
+
+
+class ScrapeOutpatientLink(Scraper):
+    """旭川市新型コロナウイルス発熱外来データExcelファイルのリンクの抽出
+
+    北海道公式ホームページから旭川市の新型コロナウイルス発熱外来データExcelファイルの
+    リンクを抽出する。
+
+    Attributes:
+        lists (list of dict): 旭川市の発熱外来データExcelファイルのURLを表す辞書のリスト
+
+    """
+
+    def __init__(self, html_url: str):
+        """
+        Args:
+            html_url (str): HTMLファイルのURL
+                北海道の発熱外来一覧一覧ページのURL
+
+        """
+        Scraper.__init__(self)
+        self.__lists = list()
+        self.__lists.append(self.get_source_excel_link(html_url))
+
+    @property
+    def lists(self) -> list:
+        return self.__lists
+
+    def get_source_excel_link(self, html_url: str) -> dict:
+        """スクレイピングの元ExcelファイルのURLを抽出して返す
+
+        Args:
+            html_url (str): HTMLファイルのURL
+
+        Returns:
+            source_excel_link (str): 発熱外来一覧ExcelファイルのURL
+
+        """
+        source_excel_link = {
+            "url": "",
+        }
+        downloaded_html = DownloadedHTML(html_url)
+        soup = BeautifulSoup(downloaded_html.content, "html.parser")
+        for article in soup.find_all("article"):
+            # id属性などで対象を絞れないためdiv要素を全てなめる。
+            # 子要素にa要素があればhref属性を取得し、それがExcelファイルへのリンクか判断。
+            # 更にa要素の子要素にimg要素がありalt属性の値に「旭川」を含んでいればリンク文字列を取得。
+            for div in soup.find_all("div"):
+                a = div.find("a")
+                if a:
+                    href = a.get("href")
+                    if re.match(r"^.*.xlsx$", href):
+                        img = div.find("img")
+                        if img:
+                            alt = img.get("alt")
+                            if alt:
+                                if re.match("^.*旭川.*$", alt):
+                                    source_excel_link["url"] = href
+
+        return source_excel_link
---
+++ tests/scrapers/test_outpatient_link.py
@@ -0,0 +1,76 @@
+import pytest
+import requests
+
+from ash_unofficial_covid19.scrapers.outpatient_link import ScrapeOutpatientLink
+
+
+class TestScrapeOutpatientLink:
+    @pytest.fixture()
+    def html_content(self):
+        return """
+<article class="body">
+    <div class="ss-alignment ss-alignment-flow">
+        <h3>「外来対応医療機関（発熱外来）」及び「陽性者（療養者）の治療に関与する医療機関」</h3>
+    </div>
+    <div class="ss-alignment ss-alignment-flow">
+        <script src="https://www.pref.hokkaido.lg.jp/js/float.js"></script>
+        <link rel="stylesheet" href="https://www.pref.hokkaido.lg.jp/css/float.css" />
+    </div>
+    <div class="ss-alignment ss-alignment-float">
+        <p><a href="/fs/8/6/1/1/8/9/3/_/%E3%80%90%E6%9C%AD%E5%B9%8C%E5%B8%82%E3%80%91%E5%A4%96%E6%9D%A5%E5%AF%BE%E5%BF%9C%E5%8C%BB%E7%99%82%E6%A9%9F%E9%96%A2(R5.6.7).pdf"
+                target="_blank"><img alt="01P_札幌.jpg" src="/fs/8/4/9/5/8/5/2/_/01P_%E6%9C%AD%E5%B9%8C.jpg" /></a>
+        </p>
+    </div>
+    <div class="ss-alignment ss-alignment-child">
+        <p><a href="/fs/8/6/1/0/4/0/9/_/%E3%80%90%E6%9C%AD%E5%B9%8C%E5%B8%82%E3%80%91%E5%A4%96%E6%9D%A5%E5%AF%BE%E5%BF%9C%E5%8C%BB%E7%99%82%E6%A9%9F%E9%96%A2(R5.6.7).xlsx"
+                target="_blank"><img alt="01E_札幌市.jpg"
+                    src="/fs/8/4/9/5/8/5/5/_/01E_%E6%9C%AD%E5%B9%8C%E5%B8%82.jpg" /></a></p>
+    </div>
+    <div class="ss-alignment ss-alignment-child">
+        <p><a href="/fs/8/6/1/1/8/9/2/_/%E3%80%90%E9%81%93%E5%A4%AE%E5%9C%B0%E5%9F%9F%E3%80%91%E5%A4%96%E6%9D%A5%E5%AF%BE%E5%BF%9C%E5%8C%BB%E7%99%82%E6%A9%9F%E9%96%A2(R5.6.7).pdf"
+                target="_blank"><img alt="05P_道央地域.jpg"
+                    src="/fs/8/4/9/5/8/5/8/_/05P_%E9%81%93%E5%A4%AE%E5%9C%B0%E5%9F%9F.jpg" /></a>
+        </p>
+    </div>
+    <div class="ss-alignment ss-alignment-child">
+        <p><a href="/fs/8/6/1/0/4/1/1/_/%E3%80%90%E9%81%93%E5%A4%AE%E5%9C%B0%E5%9F%9F%E3%80%91%E5%A4%96%E6%9D%A5%E5%AF%BE%E5%BF%9C%E5%8C%BB%E7%99%82%E6%A9%9F%E9%96%A2(R5.6.7).xlsx"
+                target="_blank"><img alt="05E_道央地域.jpg"
+                    src="/fs/8/4/9/5/8/6/1/_/05E_%E9%81%93%E5%A4%AE%E5%9C%B0%E5%9F%9F.jpg" /></a>
+        </p>
+    </div>
+    <div class="ss-alignment ss-alignment-child">
+        <p><a href="https://www.google.com/maps/d/edit?mid=1dsFPoVjnGg-65yqbtHEqDwjmogNWMN0&amp;usp=sharing"><img
+                    alt="外来対応医療機関（いわゆる発熱外来）のマップはこちらをご覧ください。" src="/fs/8/4/9/5/8/6/4/_/%E5%9B%B31-2.png" /></a></p>
+    </div>
+    <div class="ss-alignment ss-alignment-float">
+        <p><a href="/fs/8/6/1/1/8/9/1/_/%E3%80%90%E6%97%AD%E5%B7%9D%E5%B8%82%E3%80%91%E5%A4%96%E6%9D%A5%E5%AF%BE%E5%BF%9C%E5%8C%BB%E7%99%82%E6%A9%9F%E9%96%A2(R5.6.7).pdf"
+                target="_blank"><img alt="02P_旭川.jpg" src="/fs/8/4/9/5/8/6/6/_/02P_%E6%97%AD%E5%B7%9D.jpg" /></a>
+        </p>
+    </div>
+    <div class="ss-alignment ss-alignment-child">
+        <p><a href="/fs/8/6/1/1/7/8/8/_/%E3%80%90%E6%97%AD%E5%B7%9D%E5%B8%82%E3%80%91%E5%A4%96%E6%9D%A5%E5%AF%BE%E5%BF%9C%E5%8C%BB%E7%99%82%E6%A9%9F%E9%96%A2(R5.6.7).xlsx"
+                target="_blank"><img alt="02E_旭川.jpg" src="/fs/8/4/9/5/8/6/9/_/02E_%E6%97%AD%E5%B7%9D.jpg" /></a>
+        </p>
+    </div>
+    <div class="ss-alignment ss-alignment-child">
+        <p><a href="/fs/8/6/1/1/8/9/0/_/%E3%80%90%E9%81%93%E5%8D%97%E5%9C%B0%E5%9F%9F%E3%80%91%E5%A4%96%E6%9D%A5%E5%AF%BE%E5%BF%9C%E5%8C%BB%E7%99%82%E6%A9%9F%E9%96%A2(R5.6.7).pdf"
+                target="_blank"><img alt="06P_道南地域.jpg"
+                    src="/fs/8/4/9/5/8/7/2/_/06P_%E9%81%93%E5%8D%97%E5%9C%B0%E5%9F%9F.jpg" /></a>
+        </p>
+    </div>
+</article>
+"""
+
+    def test_lists(self, html_content, mocker):
+        responce_mock = mocker.Mock()
+        responce_mock.status_code = 200
+        responce_mock.headers = {"content-type": "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"}
+        responce_mock.content = html_content
+        mocker.patch.object(requests, "get", return_value=responce_mock)
+        scraper = ScrapeOutpatientLink(html_url="http://dummy.local")
+        expect = [
+            {
+                "url": "/fs/8/6/1/1/7/8/8/_/%E3%80%90%E6%97%AD%E5%B7%9D%E5%B8%82%E3%80%91%E5%A4%96%E6%9D%A5%E5%AF%BE%E5%BF%9C%E5%8C%BB%E7%99%82%E6%A9%9F%E9%96%A2(R5.6.7).xlsx",
+            },
+        ]
+        assert scraper.lists == expect
